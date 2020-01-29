6d517078b78e76e34e8ffeabb94d321381d62eb1
Greg Nass <gdnass@gmail.com>
2020-01-29 15:48:54 -0600

    step 2

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -16,6 +16,8 @@ public class Post {
     private String author;
     @Temporal(TemporalType.DATE)
     private Date date;
+    @ManyToOne
+    private Category category;
 
     public Post() {
         super();
@@ -82,5 +84,13 @@ public class Post {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
